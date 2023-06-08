234b5847844a19016e208bfc5d338061201ead78
Osaze <osareniyeosazee@gmail.com>
2023-06-08 20:46:44 +0100

    todo list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
 @extends("layout.layout")
 @section('content')  
+
         
 <div class=body>
 <h1>Hello World</h1>
+<p>welcome to my path of victory</p>
 
 </div>
 @endsection('content') 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TodosController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,21 +12,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [TodosController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/todos/mine', [TodosController::class, 'byUserId'])->middleware(['auth'])->name('mine');
+Route::get('/todos/{id}/edit', [TodosController::class, 'edit'])->middleware(['auth'])->name('edit-form');
+Route::get('/todos/create', [TodosController::class, 'create'])->middleware(['auth'])->name('create-form');
+Route::post('/todos/{id}/update', [TodosController::class, 'update'])->middleware(['auth'])->name('update');
+Route::post('/todos/add', [TodosController::class, 'store'])->middleware(['auth'])->name('add');
+Route::get('/todos/{id}', [TodosController::class, 'show'])->middleware(['auth'])->name('show');
+Route::delete('/todos/:id', [TodosController::class, 'delete'])->middleware(['auth'])->name('delete');
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/new', function () {
-    return view('new',  ['name' => 'pause']);
-});
-Route::get('/osazee', function () {
+//
 
-    $name=[
-        'type' => 'paul',
-        'base' => 'www.osazee',
-        'cooking' => 16,
-
-    ];
-    return view('osazee', $name);
-});
+require __DIR__.'/auth.php';
